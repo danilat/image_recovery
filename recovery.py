72f296cbb63e61ffa7f3f41b4ef840f296710cc5
Dani Latorre <dani@danilat.com>
2015-05-17 19:50:13 +0200

    if images destination file doesn't exist, its created

--- recovery.py
+++ recovery.py
@@ -2,12 +2,16 @@
 import os
 import shutil
 
+dstDirectory = 'images/'
+srcDirectory = '/Users/dani/Pictures/'
+
 def fileVisit(a, dir, files):
     for f in files:
-        print f
         fileName, fileExtension = os.path.splitext(f)
         if fileExtension == ".jpg" or fileExtension == ".png":
-            shutil.copyfile(os.path.join(dir, f), 'images/'+f)
+            shutil.copyfile(os.path.join(dir, f), dstDirectory + f)
 
-os.path.walk('/Users/dani/Pictures/', fileVisit, None)
+if not os.path.exists(dstDirectory):
+    os.makedirs(dstDirectory)
+os.path.walk(srcDirectory, fileVisit, None)
     
